d6221b9402cc53d9ae951126d91a75749eec748c
thiago linch <dev.thiagolinch@gmail.com>
2023-09-12 21:52:37 -0400

    openai, routes, server, dependecies

---
+++ src/routes/upload-video.ts
@@ -0,0 +1,51 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+
+import { randomUUID } from "node:crypto"
+import fs from "node:fs"
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+
+import { fastifyMultipart } from "@fastify/multipart"
+import path from "path";
+
+const pump = promisify(pipeline)
+
+
+export async function uploadVideoRoute(app: FastifyInstance) {
+    app.register(fastifyMultipart, {
+        limits: {
+            fieldSize: 1048576 * 35, //35mb
+        }
+    })
+    app.post("/videos", async (req, res) => {
+        const data = await req.file()
+
+        if(!data) {
+            return res.status(400).send({error: "No data"})
+        }
+
+        const extencion = path.extname(data.filename)
+
+        if(extencion != '.mp3') {
+            return res.status(400).send({error: "Ivalid input type!"})
+        }
+
+        const fileBasename = path.basename(data.filename, extencion)
+        const fileUploadName = `${fileBasename}-${randomUUID()}${extencion}`
+        const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
+
+        await pump(data.file, fs.createWriteStream(uploadDestination))
+
+        const video = await prisma.video.create({
+            data: {
+                name: data.filename,
+                path: uploadDestination
+            }
+        })
+
+        return {
+            video,
+        }
+    })
+} 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import { fastify } from 'fastify'
+import { prisma } from './lib/prisma'
+import { getAllPromtpsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
 
 const app = fastify()
 
-app.get("/", () => {
-    console.log("Hello world")
-})
+app.register(getAllPromtpsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
     port: 5432,
