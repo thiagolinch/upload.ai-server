2481b3681e612b7a4eca5afef3ed6321a803ab63
thiago linch <dev.thiagolinch@gmail.com>
2023-09-13 08:43:28 -0400

    openai update key

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -6,19 +6,21 @@ import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
-    app.post("/videos/:videoId/transcription", async (req, res) => {
+    app.post("/videos/:videoId/transcription", async (req) => {
         const paramsSchema = z.object({
             videoId: z.string().uuid(),
 
         })
 
         const { videoId } = paramsSchema.parse(req.params)
+        console.log(videoId)
 
         const bodySchema = z.object({
             prompt: z.string()
         })
 
         const { prompt } = bodySchema.parse(req.body)
+        console.log(prompt)
 
         const video = await prisma.video.findUniqueOrThrow({
             where: {
@@ -27,8 +29,10 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
         })
 
         const videoPath = video.path
+        console.log(videoPath)
 
         const audioReadStream = createReadStream(videoPath)
+        console.log(audioReadStream)
 
         const response = await openai.audio.transcriptions.create({
             file: audioReadStream,
@@ -39,15 +43,18 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
             prompt,
         })
 
+        const transcription = response.text
+        console.log(transcription)
+
         await prisma.video.update({
             where: {
                 id: videoId,
             },
             data: {
-                transcription: response.text
+                transcription,
             }
         })
     
-        return response.text
+        return transcription
     })
 } 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
-import { fastify } from 'fastify'
-import { prisma } from './lib/prisma'
-import { getAllPromtpsRoute } from './routes/get-all-prompts'
-import { uploadVideoRoute } from './routes/upload-video'
-import { createTranscriptionRoute } from './routes/create-transcription'
+import { fastify } from 'fastify';
+import { fastifyCors } from "@fastify/cors"
+
+import { prisma } from './lib/prisma';
+import { getAllPromtpsRoute } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
+import { createTranscriptionRoute } from './routes/create-transcription';
 
 const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*'
+  })
 
 app.register(getAllPromtpsRoute)
 app.register(uploadVideoRoute)
